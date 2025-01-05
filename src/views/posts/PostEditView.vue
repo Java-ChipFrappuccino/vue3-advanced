318fc070bccf5f1801e9314dae5396fb19ab221d
Joon <pss21242124@gmail.com>
2025-01-06 01:08:22 +0900

    25.01.06

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -46,6 +46,11 @@
       </div>
     </template>
   </PostForm>
+  <AppAlert
+    :show="showAlert"
+    :message="alertMessage"
+    :type="alertType"
+  ></AppAlert>
 </template>
 
 <script setup>
@@ -53,6 +58,7 @@ import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { getPostById, updatePost } from "@/api/posts";
 import PostForm from "@/components/posts/PostForm.vue";
+import AppAlert from "@/components/AppAlert.vue";
 const route = useRoute();
 const router = useRouter();
 const props = defineProps({
@@ -74,6 +80,7 @@ const fetchPost = async () => {
     setForm(data);
   } catch (error) {
     console.error(error);
+    vAlert("네트워크 오류!");
   }
 };
 const setForm = ({ title, content, createdAt }) => {
@@ -85,10 +92,11 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(props.id, { ...form.value });
-    router.push({
-      name: "PostDetail",
-      params: { id: props.id },
-    });
+    vAlert("수정이 완료 되었습니다!", "success");
+    // router.push({
+    //   name: "PostDetail",
+    //   params: { id: props.id },
+    // });
   } catch (error) {
     console.error(error);
   }
@@ -98,6 +106,19 @@ const goDetail = () => {
     name: "PostDetail",
     params: { id: route.params.id },
   });
+};
+
+// alert 관련
+const showAlert = ref(false);
+const alertMessage = ref("");
+const alertType = ref("");
+const vAlert = (messasge, type = "error") => {
+  showAlert.value = true;
+  alertMessage.value = messasge;
+  alertType.value = type;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 2000);
 };
 </script>
 
